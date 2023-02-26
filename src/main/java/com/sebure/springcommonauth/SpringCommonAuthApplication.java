1dabadce3ee3e0494a8d90757b16e51b1f19c110
hanumoka <amagramer@gmail.com>
2023-02-26 15:17:46 +0900

    first commit

---
+++ src/main/java/com/sebure/springcommonauth/SpringCommonAuthApplication.java
@@ -0,0 +1,13 @@
+package com.sebure.springcommonauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommonAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCommonAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sebure/springcommonauth/SpringCommonAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebure.springcommonauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommonAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
